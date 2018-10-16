a5a550c856de785659a95a9c3b098a44b35d9bf1
vinkdong <dong@wenqi.us>
2018-10-16 15:14:08 +0800

    use stdout as log info

--- log/log.go
+++ log/log.go
@@ -3,17 +3,26 @@ package log
 import (
 	"fmt"
 	"log"
+	"os"
 )
+
+var debug = false
+var std = log.New(os.Stdout, "", log.LstdFlags)
+
+func EnableDebug() {
+	debug = true
+}
+
 
 func Info(l interface{}) {
 	info := fmt.Sprintf("[%s] %v", "INFO", l)
-	log.Println(info)
+	std.Output(2, info)
 }
 
 func Infof(l string, a ...interface{}) {
 	tmp := fmt.Sprintf(l, a...)
 	info := fmt.Sprintf("[%s] %s", "INFO", tmp)
-	log.Println(info)
+	std.Output(2, info)
 }
 
 func Error(l interface{}) {
@@ -25,4 +34,19 @@ func Errorf(format string, a ...interface{}) {
 	tmp := fmt.Sprintf(format, a...)
 	err := fmt.Sprintf("[%s] %s", "Error", tmp)
 	log.Println(err)
+}
+
+func Debug(l interface{})  {
+	if debug {
+		debug := fmt.Sprintf("[%s] %v", "DEBUG", l)
+		log.Println(debug)
+	}
+}
+
+func Debugf(format string, a ...interface{}) {
+	if debug {
+		tmp := fmt.Sprintf(format, a...)
+		debug := fmt.Sprintf("[%s] %s", "Error", tmp)
+		log.Println(debug)
+	}
 }
