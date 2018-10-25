16e1d3493b4255931fa13e262c0dbc963c63cb2e
vinkdong <dong@wenqi.us>
2018-10-25 11:46:15 +0800

    support mux lock outside

--- log/log_test.go
+++ log/log_test.go
@@ -1,6 +1,9 @@
 package log
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestInfo(t *testing.T) {
 	Info("this is info")
@@ -16,4 +19,22 @@ func TestSuccess(t *testing.T)  {
 
 func TestSuccessf(t *testing.T)  {
 	Successf("this is success with args type: %s", "string")
+}
+
+func TestLock(t*testing.T) {
+	Info("this is first log next lock log")
+	Lock()
+	go func() {
+		for {
+			Info("this is sub task")
+			time.Sleep(time.Second *1)
+		}
+	}()
+	time.Sleep(time.Second *1)
+	Unlock()
+	Info("this is last log")
+	Lock()
+	time.Sleep(time.Second*3)
+	Unlock()
+	time.Sleep(time.Second*5)
 }
