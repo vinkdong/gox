5e8e20da46b9f0eec2326d15c15367f4866e808e
Vink Dong <dong@wenqi.us>
2019-10-23 15:36:55 +0800

    ADD downloader

---
+++ http/downloader/downloader_test.go
@@ -0,0 +1,17 @@
+package downloader
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	statusCode,err := Download("https://vinkdong.com/sis", "a/b/c/sxs")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if statusCode != 404 {
+		t.Fatal("status code should be 404")
+	}
+	os.RemoveAll("a")
+}
